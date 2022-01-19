df6ae00eb902ff01b4a73ecd01633d41a75f8112
JuliSol <julisolcarreras@gmail.com>
2022-01-19 14:50:58 -0300

    carrito

---
+++ src/components/Carrito.jsx
@@ -0,0 +1,7 @@
+import carrito from "./carrito.png";
+
+export default function Carrito() {
+    return <div className="ajuste-icon">
+                <img src={carrito} alt="carrito" className="tam-icon" />
+        </div>
+}
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,67 @@
+import Carrito from "./Carrito"
+import logo from "./logo.png"
+
+function NavBar() {
+    function visibleCateg() {
+        const listaCategorias = document.getElementById('list-categ')
+        listaCategorias.classList.toggle('visible')
+    
+    }
+
+    function visibleCuenta() {
+        const listaCategorias = document.getElementById('list-cuenta')
+        listaCategorias.classList.toggle('visible')
+    
+    }
+    
+    function visibleAyuda() {
+        const listaCategorias = document.getElementById('list-ayuda')
+        listaCategorias.classList.toggle('visible')
+    
+    }
+    
+    return (
+        <nav className="estilo-nav">
+            <div className="ajuste-icon">
+                <img src={logo} alt="logo" />
+            </div>
+            <div className="ajuste-icon"><p style={{fontSize: 15}}>Nombre pagina</p></div>
+            <div>
+                <ol className="menu-contenedor">
+                    <li className="orden-menu">Inicio</li>
+                    <li className="orden-menu">
+                        <p id="categorias" onClick={visibleCateg} style={{margin: 0}}>Categorias</p>
+                        <ul className="estilo-categ" id='list-categ'>
+                            <li className="padd"><a href=''>Categoria 1</a></li>
+                            <li className="padd"><a href=''>Categoria 2</a></li>
+                            <li className="padd"><a href=''>Categoria 3</a></li>
+                            <li className="padd"><a href=''>Categoria 4</a></li>
+                            <li className="padd"><a href=''>Categoria 5</a></li>
+                        </ul>
+                        <a href=''></a>
+                    </li>
+                    <li className="orden-menu">
+                        <p id="mi-cuenta" onClick={visibleCuenta} style={{margin: 0}}>Mi cuenta</p>
+                        <ul className="estilo-categ" id="list-cuenta">
+                            <li className="padd"><a href=''>Ingresar</a></li>
+                            <li className="padd"><a href=''>Facturas</a></li>
+                            <li className="padd"><a href=''>Sorteos</a></li>
+                            <li className="padd"><a href=''>Preguntas</a></li>
+                        </ul>
+                    </li>
+                    <li className="orden-menu">
+                        <p id="ayuda" onClick={visibleAyuda} style={{margin: 0}}>Ayuda</p>
+                        <ul className="estilo-categ" id='list-ayuda'>
+                            <li className="padd"><a href=''>Preguntas Frecuentes</a></li>
+                            <li className="padd"><a href=''>Quiero Preguntar</a></li>
+                            <li className="padd"><a href=''>Redes</a></li>
+                        </ul>
+                    </li>
+                </ol>
+            </div>
+            <Carrito />
+        </nav>
+    )
+}
+
+export default NavBar
