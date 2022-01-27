4cb337b8bd823662ef6363c9e1afffc709afeb69
CarrerasJulisol <89227042+CarrerasJulisol@users.noreply.github.com>
2022-01-26 22:32:32 -0300

    Agregados los detalles de los productos

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,14 +1,20 @@
 import ItemCount from "./ItemCount"
+import ItemModal from "./ItemModal"
 
 function Item({producto}) {
-
+    function abrirItem() {
+        const item = document.getElementById(producto.id)
+        item.classList.add('visible')
+   }
 
     return <div className="estilo-card">
         <h2 className="nombre-prod">{producto.nombre}</h2>
-        <img className="tam-prod" src={producto.imgURL} alt={producto.nombre}/>
+        <div className="ajuste-img">
+            <img className="tam-prod" src={producto.imgURL} alt={producto.nombre} onClick={abrirItem} />
+        </div>
         <p className="precio-prod">$ {producto.precio}</p>
-        <ItemCount stock={producto.stock} inicial={1} onAdd={0} />
+        <ItemCount stock={producto.stock} inicial={0} onAdd={0} />
+        <ItemModal prod={producto} />
     </div>
 }
-
 export default Item
